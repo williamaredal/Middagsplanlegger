775e3b1d2a4afa3ce0a81b008d3e8c35785ccba6
williamaredal <williamaredal@hotmail.com>
2023-07-23 16:30:15 +0200

    plans for recipees

--- src/Recipies.js
+++ src/Recipies.js
@@ -1,3 +1,57 @@
+// add feature where recipe has ingredients and instructions in nested dictionaries
+/* 
+    "Spagetti Bolognese" : {
+      "Ingredients" : {
+        "g karbonadedeig" : 100,
+        "ss margarin" : 0.5,
+        "stk gulrot" : 0.25, 
+        "stk løk" : 0.25,
+        "stk stangselleri" : 0.5,
+        "ss tomatpure" : 0.5,
+        "g hermetiske tomater" : 100,
+        "dl kjøttbuljong" : 0.5,
+        "ts salt" : 0.25,
+        "ts pepper" : 0.13,
+        "ts malt muskat" : 0.06,
+        "ts sukker" : 0.13,
+        "g spagetti" : 100
+      },
+      "Recipe" : "
+
+      Spagetthi Bolognese oppskrift
+
+      1. Ha margarin eller olje i en varm stekepanne. Vent til margarinen slutter å bruse, og stek karbonadedeigen i to omganger. Bruk en stekespade til å dele den i mindre biter.
+      2. Ha i hakkede grønnsaker, tomatpuré, hermetisk tomat og buljong.
+      3. Kok opp, senk varmen og la sausen småkoke i 15-20 minutter. Smak til med salt, pepper, muskat og sukker.
+      4. Kok spagetti etter anvisning på pakken.
+
+
+      "
+    },
+    "Pasta med kremet sopp" : {
+      "Ingredients" : {
+      "g sopp" : 50,
+      "stk løk" : 0.13,
+      "hvitløksbåt" : 0.25,
+      "ss smør eller margarin" : 0.5,
+      "dl matfløte" : 0.75,
+      "ss soyasaus" : 0.25,
+      "g frisk spinat" : 25,
+      "g tagliatelle" : 100
+    },
+    "Recipe" : "
+      Pasta med kremet sopp oppskrift
+
+      1. Vask og rens soppen, og del den om nødvendig i mindre biter.
+      2. Finhakk løk og hvitløk.
+      3. Smelt smør eller margarin i en stekepanne og fres sopp og løk myk og gyllen. Krydre med litt salt og pepper.
+      4. Tilsett fløte og la det hele koke opp. Smak til med soyasaus, salt og kvernet pepper.
+      5. Kok pasta etter anvisning på pakken. Bruk litt av pastavannet i sausen om den tykner for mye. 
+      6. Vend spinat inn i sausen rett før servering.
+      7. Fordel pasta i tallerkener og topp med soppsausen. Riv parmesan over.
+      "
+    },
+*/
 let Recipies = {
     "Spagetti Bolognese" : {
       "g karbonadedeig" : 100,
